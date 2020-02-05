02e860ac12aafe1977c265eed6bda86a6d456861
Raymond Loman <raymond@carpago.nl>
2020-02-05 14:19:39 +0100

    more cleanup and make the test nicer

--- src/test/java/nl/carpago/expressiondrawer/ExpressionTester.java
+++ src/test/java/nl/carpago/expressiondrawer/ExpressionTester.java
@@ -1,8 +1,9 @@
 package nl.carpago.expressiondrawer;
 
-import nl.carpago.expressiondrawer.parser.exceptions.InvalidExpressionException;
 import nl.carpago.expressiondrawer.model.Tree;
 import nl.carpago.expressiondrawer.parser.ExpressionConverter;
+import nl.carpago.expressiondrawer.parser.exceptions.InvalidExpressionException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExpressionTester {
@@ -16,25 +17,24 @@ public class ExpressionTester {
         //String expression = "axb-56*78"; //8
         //String expression = "22/7";
         //String expression = "3x((2+(5-3))";
-        //String expression = "2-3x4/2";
-        String expression = "3+-4";
+        String expression = "2-3x4/2";
+//        String expression = "3+-4";
         ExpressionConverter converter = new ExpressionConverter(expression);
-        String out = converter.getPostorderString();
-        System.out.println("<postorder>" + out + "</postorder>");
+        Assert.assertEquals("234x2/-", converter.getPostorderString());
 
-        try {
-            Tree tree = converter.getExpressionTree();
-            tree.drukAf();
-        } catch (InvalidExpressionException e) {
+        Assert.assertEquals("2-3x4/2", converter.getInorder());
 
-        }
-        //double eval = tree.eval();
-        //System.out.println(eval);
-        System.out.println("<inorder>" + converter.getInorder() + "</inorder>");
         String[] tokens = converter.getTokens();
         for (int i = 0; i < tokens.length; i++) {
             System.out.println("<token#" + i + ">" + tokens[i] + "</token>");
         }
-    }
 
+        try {
+            Tree tree = converter.getExpressionTree();
+            tree.drukAf();
+            Assert.assertEquals(-4, tree.eval(), 0.01);
+        } catch (InvalidExpressionException e) {
+            Assert.fail("Invalid expression found!"+expression);
+        }
+    }
 }
