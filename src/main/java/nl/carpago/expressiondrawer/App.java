5993dd8a9cf54f96bb928d02aa32a08a06ad197f
Raymond Loman <raymond@carpago.nl>
2020-02-05 13:25:14 +0100

    init

---
+++ src/main/java/nl/carpago/expressiondrawer/App.java
@@ -0,0 +1,13 @@
+package nl.carpago.expressiondrawer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
